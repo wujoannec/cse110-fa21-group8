586de003d86f676bc8754ededac3f93900e6b116
RyProgrammer <rpwright@ucsd.edu>
2021-11-09 13:21:43 -0800

    Create "scrolling" functionality

--- source/home-page/README.md
+++ source/home-page/README.md
@@ -4,3 +4,5 @@
 3. [Search image](https://www.google.com/url?sa=i&url=https%3A%2F%2Ficonscout.com%2Ficon%2Fsearch-2287049&psig=AOvVaw2AQ8LkcqNM7K1xZ9dGZ9tI&ust=1636406202993000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjPwaWWh_QCFQAAAAAdAAAAABAa)
 4. [Logo font](https://fonts.google.com/specimen/Lobster?preview.text=Devil%20Dishes&preview.text_type=custom#about)
 5. [Learning about fonts](https://developer.mozilla.org/en-US/docs/Learn/CSS/Styling_text/Web_fonts)
+6. [Left arrow image](https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fleft-arrow_271218&psig=AOvVaw1mxajA2-8ele9IgOd9CqnN&ust=1636568224310000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKinkfHxi_QCFQAAAAAdAAAAABAJ)
+7. [Right arrow image](https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shareicon.net%2Fadvance-soft-icons-arrow-right-arrows-symbol-right-arrow-pointing-direction-directional-681008&psig=AOvVaw3MRew4sSOMHZovlHIv-l98&ust=1636568273697000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLjf9Iryi_QCFQAAAAAdAAAAABAD)
--- source/home-page/scripts/home.js
+++ source/home-page/scripts/home.js
@@ -1,5 +1,71 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    // Add some functionality to the page.
+    const recipes = [
+        'assets/sample-recipes/burger.jpg',
+        'assets/sample-recipes/french-toast.jpg',
+        'assets/sample-recipes/fried-ice-cream.jpg',
+        'assets/sample-recipes/fries.jpg',
+        'assets/sample-recipes/hot-coacoa.jpg',
+        'assets/sample-recipes/mochi.jpg',
+        'assets/sample-recipes/pizza.jpg',
+        'assets/sample-recipes/quesa.jpg',
+        'assets/sample-recipes/ravioli.jpg',
+        'assets/sample-recipes/sushi.jpg',
+        'assets/sample-recipes/sweet-rice.jpg'
+    ]
+    const recipeGrid = document.querySelector('.recipe-grid');
+    const recipeElements = document.querySelectorAll('.recipe');
+    const recipeWH = 60;
+    var pointer = 0;
+
+    fillGrid();
+
+    function fillGrid() {
+        // Remove current recipes on display
+        for(let i = 0; i < recipeElements.length; i++) {
+            if(recipeElements[i].children.length > 0) {
+                if(recipeElements[i].children[0].tagName == 'IMG') {
+                    recipeElements[i].removeChild(recipeElements[i].children[0]);
+                    recipeElements[i].textContent = "<EMPTY>"
+                }
+            }
+        }
+
+        // Add new recipes to display
+        let capacity = pointer;
+
+        for(let i = capacity % recipeElements.length; i < recipeElements.length; i++) {
+            if(pointer >= recipes.length) break;
+            
+            // Create recipe element
+            const recipe = document.createElement('img');
+            recipe.setAttribute('src', recipes[pointer]);
+            recipe.setAttribute('width', recipeWH);
+            recipe.setAttribute('height', recipeWH);
+
+            // Add given recipe
+            recipeElements[i].textContent = recipes[pointer].substr(22);
+            recipeElements[i].appendChild(recipe);
+
+            // Update pointer
+            pointer++;
+        }
+
+        pointer = capacity + recipeElements.length;
+        console.log(pointer);
+    }
+
+    const rightButton = document.getElementById('right');
+    rightButton.addEventListener('click', e => {
+        if (pointer < recipes.length) fillGrid();
+    });
+
+    const leftButton = document.getElementById('left');
+    leftButton.addEventListener('click', e => {
+        if(pointer > recipeElements.length) {
+            pointer -= recipeElements.length * 2;
+            fillGrid();
+        }
+    });
 }
