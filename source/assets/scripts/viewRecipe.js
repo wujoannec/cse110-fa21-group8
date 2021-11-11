131c114bedb7f268fa6c3c7f96ae7bb831900f5b
Em Nam <enam@ucsd.edu>
2021-11-11 13:15:12 -0800

    organize html id/classes, create grid
    Co-authored-by: Jonny Tran <61168867+jonathanktran@users.noreply.github.com>

--- source/assets/scripts/viewRecipe.js
+++ source/assets/scripts/viewRecipe.js
@@ -1,12 +1,12 @@
 let favButton = document.getElementById('favBtn');
 favButton.addEventListener('click', function () 
 {
-    if (favButton.getAttribute('src') == 'assets/images/heart-empty.png')
+    if (favButton.getAttribute('src') == 'assets/images/heartEmpty.png')
     {
-        favButton.setAttribute('src', 'assets/images/heart-full.png');
+        favButton.setAttribute('src', 'assets/images/heartFull.png');
     }
     else 
     {
-        favButton.setAttribute('src', 'assets/images/heart-empty.png');
+        favButton.setAttribute('src', 'assets/images/heartEmpty.png');
     }
 });
