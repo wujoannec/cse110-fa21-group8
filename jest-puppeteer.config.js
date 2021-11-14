ada22f6fdfd227f1ad9e6bbb53e4de50871bfae0
tianyuezhang1997 <tianyuezhang1997@gmail.com>
2021-11-14 08:43:58 -0800

    Apply --no-sandbox option for browser

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-  launch: {
+const puppeteer = require('puppeteer');
+module.exports = async function () {
+  // According to
+  // (1) https://jestjs.io/docs/puppeteer
+  // (2) https://developers.google.com/web/tools/puppeteer/troubleshooting
+  // (3) https://stackoverflow.com/questions/62228154/puppeteer-fails-to-initiate-in-github-actions
+  browser = await puppeteer.launch({
     headless: false,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
     slowMo: 25,
-  },
+  });
 };
