b24c026704ad09d7031fee2efcd688b4f83efb83
Em Nam <enam@ucsd.edu>
2021-11-18 16:09:47 -0800

    create html skeletons, add assets
    Co-authored-by: Jonny Tran <61168867+jonathanktran@users.noreply.github.com>

---
+++ source/assets/scripts/viewRecipe.js
@@ -0,0 +1,8 @@
+let favButton = document.getElementById("favBtn");
+favButton.addEventListener("click", function () {
+  if (favButton.getAttribute("src") == "assets/images/heartEmpty.png") {
+    favButton.setAttribute("src", "assets/images/heartFull.png");
+  } else {
+    favButton.setAttribute("src", "assets/images/heartEmpty.png");
+  }
+});
