abe6d39342132a437402dc3088dee45fce170627
Em Nam <enam@ucsd.edu>
2021-11-20 23:05:49 -0800

    change + button, limit author input

--- source/assets/scripts/editRecipe.js
+++ source/assets/scripts/editRecipe.js
@@ -3,10 +3,22 @@ let tags = document.querySelectorAll(".tags > *");
 tags.forEach(tag => {
     tag.addEventListener('click', function(){
         console.log('click');
+        
         if(tag.classList.contains('selected')){
             tag.classList.remove('selected');
         } else {
             tag.classList.add('selected');
         }
     });
+});
+
+var maxLength = 20;
+
+let author = document.getElementById("author");
+console.log(author.innerHTML);
+author.addEventListener('keydown', function(event){
+    console.log(author.innerHTML);
+    if (author.innerHTML.length > maxLength && event.keyCode != 8){
+        event.preventDefault();
+    }
 });
