d4b32e80648cacd1e23643344e2c52432c328845
Em Nam <enam@ucsd.edu>
2021-11-20 23:08:10 -0800

    add js comments

--- source/assets/scripts/editRecipe.js
+++ source/assets/scripts/editRecipe.js
@@ -1,3 +1,4 @@
+// Allow each tag to be toggled to selected class or not
 let tags = document.querySelectorAll(".tags > *");
 
 tags.forEach(tag => {
@@ -12,13 +13,14 @@ tags.forEach(tag => {
     });
 });
 
-var maxLength = 20;
 
+// Limit user character input to author
+var maxLength = 10;
 let author = document.getElementById("author");
 console.log(author.innerHTML);
 author.addEventListener('keydown', function(event){
     console.log(author.innerHTML);
-    if (author.innerHTML.length > maxLength && event.keyCode != 8){
-        event.preventDefault();
+    if (author.innerHTML.length > maxLength && event.keycode != 8){
+        e.preventDefault();
     }
 });
