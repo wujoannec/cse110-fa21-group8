cc75dfa201c7ac0286fe3a2c59dc1080e99cf8eb
tianyuezhang1997 <tianyuezhang1997@gmail.com>
2021-11-23 13:37:30 -0800

    Update submit-my-work.sh

--- submit-my-work.sh
+++ submit-my-work.sh
@@ -14,15 +14,15 @@ push_result=`git push origin`
 
 if [[ ${deploy_result} != *"completed"* ]]; then
     echo "Error! Failed deploying to the testing server."
-    else
-        echo "Succeed! Great job! Feel free to visit : "
-        echo "         https://testing.tianyuezhang1997.site/cse110-fa21-group8/${currBranch}"
-        echo "to see your deployed work."
-fi
-if [[ ${push_result} != *"completed"* ]]; then
+    if [[ ${push_result} != *"completed"* ]]; then
     echo "Error! Failed pushing to the team repo."
     else
         echo "Succeed! Great job! Feel free to visit : "
         echo "         https://github.com/cse110-fa21-group8/cse110-fa21-group8/actions"
         echo "to see the testing result of your work"
+    fi
+    else
+        echo "Succeed! Great job! Feel free to visit : "
+        echo "         https://testing.tianyuezhang1997.site/cse110-fa21-group8/${currBranch}"
+        echo "to see your deployed work."
 fi
