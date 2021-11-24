a8cab58b889672eb8457a8c1e7790f14609760fd
UCSDJIM <zjliang@ucsd.edu>
2021-11-24 15:26:25 -0800

    add new test and tools

--- source/assets/scripts/viewRecipe.js
+++ source/assets/scripts/viewRecipe.js
@@ -1,21 +1,20 @@
 // toggle favorites button
-let favBtn = document.getElementById('favBtn');
-favBtn.addEventListener('click', function () {
-    if (favBtn.getAttribute('src') == '../source/assets/images/heartEmpty.png') {
-        favBtn.setAttribute('src', '../source/assets/images/heartFull.png');
-    }
-    else {
-        favBtn.setAttribute('src', '../source/assets/images/heartEmpty.png');
-    }
+let favBtn = document.getElementById("favBtn");
+favBtn.addEventListener("click", function () {
+  if (favBtn.getAttribute("src") == "../source/assets/images/heartEmpty.png") {
+    favBtn.setAttribute("src", "../source/assets/images/heartFull.png");
+  } else {
+    favBtn.setAttribute("src", "../source/assets/images/heartEmpty.png");
+  }
 });
 
 // move to edit recipe page
-let editBtn = document.getElementById('editBtn');
-editBtn.addEventListener('click', function () {
-    window.location = 'editRecipe.html';
+let editBtn = document.getElementById("editBtn");
+editBtn.addEventListener("click", function () {
+  window.location = "editRecipe.html";
 });
 
-let backBtn = document.getElementById('backBtn');
-backBtn.addEventListener('click', function () {
-    window.location = 'index.html';
+let backBtn = document.getElementById("backBtn");
+backBtn.addEventListener("click", function () {
+  window.location = "index.html";
 });
