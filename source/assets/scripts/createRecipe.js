958631d02b82c2b81379da7917555fd6c802c303
tianyuezhang1997 <tianyuezhang1997@gmail.com>
2021-11-24 20:42:40 -0800

    Fix coding style

--- source/assets/scripts/createRecipe.js
+++ source/assets/scripts/createRecipe.js
@@ -1,70 +1,70 @@
 // toggle tags
 let tags = document.querySelectorAll(".tags > *");
-tags.forEach(tag => {
-    tag.addEventListener('click', function () {
-        if (tag.classList.contains('selected')) {
-            tag.classList.remove('selected');
-        } else {
-            tag.classList.add('selected');
-        }
-    });
+tags.forEach((tag) => {
+  tag.addEventListener("click", function () {
+    if (tag.classList.contains("selected")) {
+      tag.classList.remove("selected");
+    } else {
+      tag.classList.add("selected");
+    }
+  });
 });
 
 // return to view recipe page without making edits
-let backBtn = document.getElementById('backBtn');
-backBtn.addEventListener('click', function () {
-    window.location = '../index.html';
+let backBtn = document.getElementById("backBtn");
+backBtn.addEventListener("click", function () {
+  window.location = "../index.html";
 });
 // return to view recipe page without making edits
-let confirmBtn = document.getElementById('confirmBtn');
-confirmBtn.addEventListener('click', function () {
-    window.location = 'viewRecipe.html';
+let confirmBtn = document.getElementById("confirmBtn");
+confirmBtn.addEventListener("click", function () {
+  window.location = "viewRecipe.html";
 });
 
-let ingredientBtns = document.getElementById('ingredientBtns');
+let ingredientBtns = document.getElementById("ingredientBtns");
 // add ingredient text field
-let addIngredient = document.getElementById('addIngredient');
-addIngredient.addEventListener('click', function () {
-    let newIngredient = document.createElement('div');
-    newIngredient.setAttribute('role', 'textbox');
-    newIngredient.setAttribute('contenteditable', 'true');
-    newIngredient.setAttribute('id', 'newInstruction');
-    ingredientBtns.insertAdjacentElement("beforebegin", newIngredient);
+let addIngredient = document.getElementById("addIngredient");
+addIngredient.addEventListener("click", function () {
+  let newIngredient = document.createElement("div");
+  newIngredient.setAttribute("role", "textbox");
+  newIngredient.setAttribute("contenteditable", "true");
+  newIngredient.setAttribute("id", "newInstruction");
+  ingredientBtns.insertAdjacentElement("beforebegin", newIngredient);
 });
 // remove ingredient text field
-let removeIngredient = document.getElementById('removeIngredient');
-removeIngredient.addEventListener('click', function () {
-    let toDelete = ingredientBtns.previousElementSibling;
-    if (toDelete.nodeName == "DIV") {
-        toDelete.remove();
-    }
+let removeIngredient = document.getElementById("removeIngredient");
+removeIngredient.addEventListener("click", function () {
+  let toDelete = ingredientBtns.previousElementSibling;
+  if (toDelete.nodeName == "DIV") {
+    toDelete.remove();
+  }
 });
 
-let instructionBtns = document.getElementById('instructionBtns');
+let instructionBtns = document.getElementById("instructionBtns");
 // add ingredient text field
-let addInstruction = document.getElementById('addInstruction');
-addInstruction.addEventListener('click', function () {
-    let newInstruction = document.createElement('div');
-    newInstruction.setAttribute('role', 'textbox');
-    newInstruction.setAttribute('contenteditable', 'true');
-    newInstruction.setAttribute('id', 'newInstruction');
-    instructionBtns.insertAdjacentElement("beforebegin", newInstruction);
+let addInstruction = document.getElementById("addInstruction");
+addInstruction.addEventListener("click", function () {
+  let newInstruction = document.createElement("div");
+  newInstruction.setAttribute("role", "textbox");
+  newInstruction.setAttribute("contenteditable", "true");
+  newInstruction.setAttribute("id", "newInstruction");
+  instructionBtns.insertAdjacentElement("beforebegin", newInstruction);
 });
 // remove ingredient text field
-let removeInstruction = document.getElementById('removeInstruction');
-removeInstruction.addEventListener('click', function () {
-    let toDelete = instructionBtns.previousElementSibling;
-    if (toDelete.nodeName == "DIV") {
-        toDelete.remove();
-    }
+let removeInstruction = document.getElementById("removeInstruction");
+removeInstruction.addEventListener("click", function () {
+  let toDelete = instructionBtns.previousElementSibling;
+  if (toDelete.nodeName == "DIV") {
+    toDelete.remove();
+  }
 });
 
 // change recipe image on upload
-let recipeImg = document.getElementById('recipeImg');
-let uploadImg = document.getElementById('uploadImg');
-uploadImg.addEventListener('change', function () {
-    let img = uploadImg.files[0];
-    if (img) {
-        recipeImg.setAttribute('src', URL.createObjectURL(img));
-    }
+let recipeImg = document.getElementById("recipeImg");
+let uploadImg = document.getElementById("uploadImg");
+uploadImg.addEventListener("change", function () {
+  let img = uploadImg.files[0];
+  if (img) {
+    recipeImg.setAttribute("src", URL.createObjectURL(img));
+  }
 });
--- source/assets/scripts/home.js
+++ source/assets/scripts/home.js
@@ -1,100 +1,107 @@
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 async function init() {
-    // Create Recipes (took some code from Lab 5)
-    const recipes = [];
-    var dataBank;
+  // Create Recipes (took some code from Lab 5)
+  const recipes = [];
+  var dataBank;
 
-    async function fetchRecipes() {
-        return new Promise((resolve, reject) => {  
-            let json = (window.location['pathname'].includes("/views/explorePage.html")) ? "../source/assets/json/dataDel.json" : "./source/assets/json/data.json";
-            fetch(json)
-                .then(response => response.json())
-                .then(data => {
-                    let ind = 0;
-                    for(let recipe in data) {
-                        recipes[ind] = data[recipe];
-                        ind++;
-                    }
-                    console.log(data);
-                    resolve(true);
-                })
-                .catch(error => reject(false));
-        });
+  async function fetchRecipes() {
+    return new Promise((resolve, reject) => {
+      let json = window.location["pathname"].includes("/views/explorePage.html")
+        ? "../source/assets/json/dataDel.json"
+        : "./source/assets/json/data.json";
+      fetch(json)
+        .then((response) => response.json())
+        .then((data) => {
+          let ind = 0;
+          for (let recipe in data) {
+            recipes[ind] = data[recipe];
+            ind++;
+          }
+          console.log(data);
+          resolve(true);
+        })
+        .catch((error) => reject(false));
+    });
+  }
+
+  let fetchSuccessful = await fetchRecipes();
+
+  if (!fetchSuccessful) {
+    console.log("Recipe fetch unsuccessful");
+    return;
+  }
+
+  const recipeGrid = document.querySelector(".recipe-grid");
+  const recipeElements = document.querySelectorAll(".recipe");
+  const recipeWH = "170vw";
+  var pointer = 0;
+
+  fillGrid();
+
+  function fillGrid() {
+    // Remove current recipes on display
+    for (let i = 0; i < recipeElements.length; i++) {
+      if (recipeElements[i].children.length > 0) {
+        if (recipeElements[i].children[0].tagName == "IMG") {
+          recipeElements[i].removeChild(recipeElements[i].children[0]);
+          recipeElements[i].textContent = "\r\n<EMPTY>";
+        }
+      }
     }
 
-    let fetchSuccessful = await fetchRecipes();
+    // Add new recipes to display
+    let capacity = pointer;
 
-    if (!fetchSuccessful) {
-        console.log('Recipe fetch unsuccessful');
-        return;
-    };
+    for (
+      let i = capacity % recipeElements.length;
+      i < recipeElements.length;
+      i++
+    ) {
+      if (pointer >= recipes.length) break;
 
-    const recipeGrid = document.querySelector('.recipe-grid');
-    const recipeElements = document.querySelectorAll('.recipe');
-    const recipeWH = '170vw';
-    var pointer = 0;
+      // Create recipe element
+      const recipe = document.createElement("img");
+      recipe.setAttribute("src", recipes[pointer]);
+      recipe.setAttribute("width", recipeWH);
+      recipe.setAttribute("height", recipeWH);
+      // recipe.style.position = "absolute";
+      // recipe.style.marginTop = "-7em";
+      // recipe.style.marginLeft = "-5em";
 
-    fillGrid();
+      // recipe.style.xIndex = "200px"
 
-    function fillGrid() {
-        // Remove current recipes on display
-        for(let i = 0; i < recipeElements.length; i++) {
-            if(recipeElements[i].children.length > 0) {
-                if(recipeElements[i].children[0].tagName == 'IMG') {
-                    recipeElements[i].removeChild(recipeElements[i].children[0]);
-                    recipeElements[i].textContent = "\r\n<EMPTY>"
-                }
-            }
-        }
+      // Add given recipe
+      recipeElements[i].textContent =
+        "\r\n" +
+        recipes[pointer].slice(32, recipes[pointer].length - 4) +
+        "\r\n\n";
+      //recipeElements[i].textContent = '\r\n' + recipes[pointer].substr(32, recipes[pointer].substr(22).length - 4).replace('-', ' ')+'\r\n\n';
+      recipeElements[i].appendChild(recipe);
 
-        // Add new recipes to display
-        let capacity = pointer;
-
-        for(let i = capacity % recipeElements.length; i < recipeElements.length; i++) {
-            if(pointer >= recipes.length) break;
-            
-            // Create recipe element
-            const recipe = document.createElement('img');
-            recipe.setAttribute('src', recipes[pointer]);
-            recipe.setAttribute('width', recipeWH);
-            recipe.setAttribute('height', recipeWH);
-            // recipe.style.position = "absolute";
-            // recipe.style.marginTop = "-7em";
-            // recipe.style.marginLeft = "-5em";
-
-
-            // recipe.style.xIndex = "200px"
-
-
-            // Add given recipe
-            recipeElements[i].textContent = '\r\n' +  recipes[pointer].slice(32, recipes[pointer].length - 4) + '\r\n\n';
-            //recipeElements[i].textContent = '\r\n' + recipes[pointer].substr(32, recipes[pointer].substr(22).length - 4).replace('-', ' ')+'\r\n\n';
-            recipeElements[i].appendChild(recipe);
-
-            // Update pointer
-            pointer++;
-        }
-
-        pointer = capacity + recipeElements.length;
-        console.log(pointer);
+      // Update pointer
+      pointer++;
     }
 
-    const rightButton = document.getElementById('right');
-    rightButton.addEventListener('click', e => {
-        if (pointer < recipes.length) fillGrid();
-    });
+    pointer = capacity + recipeElements.length;
+    console.log(pointer);
+  }
 
-    const leftButton = document.getElementById('left');
-    leftButton.addEventListener('click', e => {
-        if(pointer > recipeElements.length) {
-            pointer -= recipeElements.length * 2;
-            fillGrid();
-        }
-    });
+  const rightButton = document.getElementById("right");
+  rightButton.addEventListener("click", (e) => {
+    if (pointer < recipes.length) fillGrid();
+  });
 
-    const createButton = document.getElementById('createButton');
-    createButton.addEventListener('click', function() {
-        window.location = 'createRecipe.html';
-    })
+  const leftButton = document.getElementById("left");
+  leftButton.addEventListener("click", (e) => {
+    if (pointer > recipeElements.length) {
+      pointer -= recipeElements.length * 2;
+      fillGrid();
+    }
+  });
+
+  const createButton = document.getElementById("createButton");
+  createButton.addEventListener("click", function () {
+    window.location = "createRecipe.html";
+  });
 }
