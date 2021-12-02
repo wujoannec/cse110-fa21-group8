8f2e53166c860fda60689491012e6b16298ffbb9
Em Nam <78702559+Em-Nam@users.noreply.github.com>
2021-12-02 15:56:30 -0800

    combine fork with main

--- source/assets/scripts/register.js
+++ source/assets/scripts/register.js
@@ -1,4 +1,4 @@
-import {authenticate} from "./CRUD.js"
+import {authenticate} from "./CRUD.js";
 const auth_type = "register";
 
 let userName = document.getElementById("username");
@@ -6,12 +6,14 @@ let passWord = document.getElementById("password");
 let reg = document.getElementById("submit");
 let error = document.getElementById("errorMsg");
 
-reg.addEventListener("click", async function() {
-    let res = await authenticate(userName.value, passWord.value, auth_type)
-                        .then(resolve => {return resolve});
-    error.style.display = "block";
-    error.innerHTML = res;
-    console.log("error displayed");
-    console.log(res);
+reg.addEventListener("click", async function () {
+  let res = await authenticate(userName.value, passWord.value, auth_type).then(
+    (resolve) => {
+      return resolve;
+    }
+  );
+  error.style.display = "block";
+  error.innerHTML = res;
+  console.log("error displayed");
+  console.log(res);
 });
-
